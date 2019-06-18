20452120e3c7da26aca2188f23e92933ebbf1a73
KidMongoose <kidmongoose@gmail.com>
2019-06-17 22:22:17 -0700

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -1,5 +1,32 @@
 #!/usr/bin/python3
 
+'''
+    Here's a list of words you can try, courtesy of these nice chaps: 
+    https://examples.yourdictionary.com/palindrome-examples.html 
+
+    Madam
+    Mom
+    Noon
+    Racecar
+    Radar
+    Redder
+    Refer
+    Repaper
+    Rotator
+    Rotor
+    
+    Don't nod.
+    I did, did I?
+    My gym
+    Red rum, sir, is murder
+    Step on no pets
+    Top spot
+    Was it a cat I saw?
+    Eva, can I see bees in a cave?
+    No lemon, no melon
+ 
+'''
+
 entered_value =  input('Enter a single word or multiple words to see if they\'re palindromes: ').lower()
 palindrome = ''.join(entered_value.split()).replace("'", "")
 reversedString = palindrome[::-1]
