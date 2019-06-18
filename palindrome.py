29ca16813298568f4ec96612f29b579e90a016c4
KidMongoose <kidmongoose@gmail.com>
2019-06-17 22:15:24 -0700

    Create palindrome.py

---
+++ palindrome.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+entered_value =  input('Enter a single word or multiple words to see if they\'re palindromes: ').lower()
+palindrome = ''.join(entered_value.split()).replace("'", "")
+reversedString = palindrome[::-1]
+
+if palindrome == reversedString:
+    print("Word or phrase is a palindrome.")
+else:
+    print("Not a palindrome. {}".format(entered_value))    
